2d5c0873c70e740ca9d2765b50f247bb7ec8bf9b
Nigel Andrews <nigel.andrews@epita.fr>
2022-09-12 20:39:56 +0200

    refactor search_token_end

--- quickwit-doc-mapper/src/tokenizers.rs
+++ quickwit-doc-mapper/src/tokenizers.rs
@@ -27,13 +27,13 @@ use tantivy::tokenizer::{
 };
 
 
-static NUMBER_REGEX: usize = 2;
 static REGEX_ARRAY: [Lazy<Regex>; 3] =
     [
         Lazy::new(|| Regex::new("^[a-z]+:?/?/?[a-z0-9]+[-_\\./a-z]*\\.[a-z]+[-/a-z._=]+").unwrap()),
         Lazy::new(|| Regex::new("^[A-Za-z]{1,3}[ \t]{1,2}[0-9]{1,2}[ \t]{1,2}[0-9]{1,4}[-_/:][0-9]{1,2}[-_/:][0-9]{1,4}").unwrap()),
         Lazy::new(|| Regex::new("^[0-9][-/%_\\.:a-zA-Z0-9]*").unwrap())
     ];
+static VALID_CHARS: &str = "%/-.";
 
 /// Log friendly Tokenizer that avoids splittings on IPs, dates, URLs and IDs
 #[derive(Clone)]
@@ -59,11 +59,8 @@ impl Tokenizer for LogTokenizer {
 impl<'a> LogTokenStream<'a> {
     fn search_token_end(&mut self) -> usize {
         (&mut self.chars)
-            // TODO Refactor
-            .filter(|&(_, ref c)| {
-                // Characters we probably don't want to split on aside from alphanumeric
-                *c != '%' && *c != '/' && *c != '-' && *c != '.' && !c.is_alphanumeric()
-            })
+            // Characters we probably don't want to split on aside from alphanumeric
+            .filter(|&(_, ref c)| !VALID_CHARS.contains(*c) && !c.is_alphanumeric())
             .map(|(offset, _)| offset)
             .next()
             .unwrap_or(self.text.len())

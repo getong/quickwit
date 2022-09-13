84405cca27f9a7a28234769d4ee3e726efb76c1c
Nigel Andrews <nigel.andrews@epita.fr>
2022-09-14 01:47:25 +0200

    updated doc and comments, added/updated tests, refactor/simplified behaviour

--- quickwit-doc-mapper/src/tokenizers.rs
+++ quickwit-doc-mapper/src/tokenizers.rs
@@ -26,27 +26,35 @@ use tantivy::tokenizer::{
     TokenizerManager,
 };
 
-static REGEX_ARRAY: Lazy<[Regex; 3]> = Lazy::new(|| {
+// The array is ordered according to how often we'd stumble upon the expression
+static REGEX_ARRAY: Lazy<[Regex; 4]> = Lazy::new(|| {
     [
-        // URL regex to match http(s) links or www links
-        Regex::new("^[a-z]+:?/?/?[a-z0-9]+[-_\\./a-z]*\\.[a-z]+[-/a-z._=]+").expect("Failed to compile regular expression. This should never happen! Please, report on https://github.com/quickwit-oss/quickwit/issues."),
+        // Regex to match numbers or numbers with certain characters that should not be split on (e.g IP addresses)
+        Regex::new("^[0-9][-/%_\\.:a-zA-Z0-9]*")
+            .expect("Failed to compile regular expression. This should never happen! Please, report on https://github.com/quickwit-oss/quickwit/issues."),
 
         // Date regex to match dates similar to this example "Dec 10 06:55:48",
         Regex::new(
-            "^[A-Za-z]{1,3}[ \t]{1,2}[0-9]{1,2}[ \
-             \t]{1,2}[0-9]{1,4}[-_/:][0-9]{1,2}[-_/:][0-9]{1,4}",
+            "^[A-Za-z]{1,3}[ \t]{1,2}[0-9]{1,2}[ \t]{1,2}[-_/:0-9]+",
              )
             .expect("Failed to compile regular expression. This should never happen! Please, report on https://github.com/quickwit-oss/quickwit/issues."),
 
-        // Regex to match numbers or numbers with certain chars (e.g IP addresses)
-        Regex::new("^[0-9][-/%_\\.:a-zA-Z0-9]*").expect("Failed to compile regular expression. This should never happen! Please, report on https://github.com/quickwit-oss/quickwit/issues.")
+        // Regex to match filesystem paths or endpoints
+        Regex::new("^/?[-a-zA-Z0-9./:%_]+[a-zA-Z0-9]+/?")
+            .expect("Failed to compile regular expression. This should never happen! Please, report on https://github.com/quickwit-oss/quickwit/issues."),
+
+        // URL regex to match http(s) or www links
+        Regex::new("^[a-z]+:?/?/?[a-z0-9]+[-_\\./a-z]*\\.[a-z]+[-/a-z._=]+")
+            .expect("Failed to compile regular expression. This should never happen! Please, report on https://github.com/quickwit-oss/quickwit/issues."),
     ]
 });
 
-static VALID_CHARS: &str = ".-/:%";
-
 /// Log friendly Tokenizer that avoids splittings on ponctuation in:
 ///     - IP addresses (both ipv4 and ipv6)
+///     - Date formats (ISO 8601, "MMM d yyyy" etc...)
+///     - Common characters found in identifiers (".", "-", etc...)
+///
+/// The tokenizer still works in full text cases such as log messages
 #[derive(Clone)]
 pub struct LogTokenizer;
 
@@ -68,20 +76,15 @@ impl Tokenizer for LogTokenizer {
 }
 
 impl<'a> LogTokenStream<'a> {
-    // Characters we probably don't want to split on aside from alphanumeric
     fn search_token_end(&mut self) -> usize {
         (&mut self.chars)
-            .filter(|&(_, ref c)| !VALID_CHARS.contains(*c) && !c.is_alphanumeric())
-            .map(|(offset, _)| offset)
-            .next()
+            .find_map(|(offset, ref character)| (!character.is_alphanumeric()).then_some(offset))
             .unwrap_or(self.text.len())
     }
 
     fn handle_match(&mut self, offset_to: usize) -> usize {
         (&mut self.chars)
-            .filter(|(index, _)| *index == offset_to)
-            .map(|(offset, _)| offset)
-            .next()
+            .find_map(|(index, _)| (index == offset_to).then_some(index))
             .unwrap_or(self.text.len())
     }
 
@@ -101,6 +104,8 @@ impl<'a> TokenStream for LogTokenStream<'a> {
         while let Some((offset_from, c)) = self.chars.next() {
             let text_substring = &self.text[offset_from..];
 
+            // First, try to find a pattern where splitting on ponctuation isn't ideal from
+            // the array of expressions
             for regex in REGEX_ARRAY.iter() {
                 if let Some(regex_match) = regex.find(text_substring) {
                     let offset_to = self.handle_match(offset_from + regex_match.end());
@@ -110,9 +115,13 @@ impl<'a> TokenStream for LogTokenStream<'a> {
                 }
             }
 
-            // Catch all condition for words and '/' in case we stumble upon a
-            // filesystem path or endpoints
-            if c.is_alphabetic() || c == '/' {
+            // This case is when we don't have a regex match so the default behaviour
+            // is needed.
+            // Either 'c' is alphabetic and should be tokenized accordingly, or
+            // it is a ponctuation character and should be ignored
+            // ('c' cannot be a digit at this point since it would have been matched
+            // by an expression in the array above)
+            if c.is_alphabetic() {
                 let offset_to = self.search_token_end();
                 self.push_token(offset_from, offset_to);
 
@@ -148,28 +157,45 @@ fn get_quickwit_tokenizer_manager() -> TokenizerManager {
 pub static QUICKWIT_TOKENIZER_MANAGER: Lazy<TokenizerManager> =
     Lazy::new(get_quickwit_tokenizer_manager);
 
-#[test]
-fn raw_tokenizer_test() {
-    let my_haiku = r#"
-        white sandy beach
-        a strong wind is coming
-        sand in my face
-        "#;
-    let my_long_text = "a text, that is just too long, no one will type it, no one will like it, \
-                        no one shall find it. I just need some more chars, now you may not pass.";
-
-    let tokenizer = get_quickwit_tokenizer_manager().get("raw").unwrap();
-    let mut haiku_stream = tokenizer.token_stream(my_haiku);
-    assert!(haiku_stream.advance());
-    assert!(!haiku_stream.advance());
-    assert!(!tokenizer.token_stream(my_long_text).advance());
-}
-
 #[cfg(test)]
 mod tests {
     use tantivy::tokenizer::{SimpleTokenizer, TextAnalyzer};
 
     use crate::tokenizers::get_quickwit_tokenizer_manager;
+
+    #[test]
+    fn raw_tokenizer_test() {
+        let my_haiku = r#"
+        white sandy beach
+        a strong wind is coming
+        sand in my face
+        "#;
+        let my_long_text = "a text, that is just too long, no one will type it, no one will like it, \
+                        no one shall find it. I just need some more chars, now you may not pass.";
+
+        let tokenizer = get_quickwit_tokenizer_manager().get("raw").unwrap();
+        let mut haiku_stream = tokenizer.token_stream(my_haiku);
+        assert!(haiku_stream.advance());
+        assert!(!haiku_stream.advance());
+        assert!(!tokenizer.token_stream(my_long_text).advance());
+    }
+
+    // Compare expected tokens in array_ref with actual tokens in stream from test_string
+    fn log_tokenizer_test_helper(test_string: &str, array_ref: &[&str]) {
+        let mut token_stream = get_quickwit_tokenizer_manager()
+            .get("log")
+            .unwrap()
+            .token_stream(test_string);
+
+        array_ref.iter().for_each(|ref_token| {
+            if token_stream.advance() {
+                assert_eq!(&token_stream.token().text, ref_token)
+            } else {
+                panic!()
+            }
+        });
+    }
+
 
     #[test]
     fn log_tokenizer_basic_test() {
@@ -183,18 +209,7 @@ mod tests {
             "02:51",
         ];
 
-        let mut token_stream = get_quickwit_tokenizer_manager()
-            .get("log")
-            .unwrap()
-            .token_stream(numbers);
-
-        array_ref.iter().for_each(|ref_token| {
-            if token_stream.advance() {
-                assert_eq!(&token_stream.token().text, ref_token)
-            } else {
-                panic!()
-            }
-        });
+        log_tokenizer_test_helper(numbers, &array_ref)
     }
 
     // The only difference with the default tantivy is within numbers, this test is
@@ -202,6 +217,29 @@ mod tests {
     #[test]
     fn log_tokenizer_compare_with_simple() {
         let test_string = "this,is,the,test 42 here\n3932\t20dk,3093raopxa'wd";
+        let mut token_stream = get_quickwit_tokenizer_manager()
+            .get("log")
+            .unwrap()
+            .token_stream(test_string);
+        let mut ref_token_stream = TextAnalyzer::from(SimpleTokenizer).token_stream(test_string);
+
+        while token_stream.advance() && ref_token_stream.advance() {
+            assert_eq!(&token_stream.token().text, &ref_token_stream.token().text);
+        }
+
+        assert!(!(token_stream.advance() || ref_token_stream.advance()));
+    }
+
+    // The tokenizer should still be able to work on normal texts
+    #[test]
+    fn log_tokenizer_basic_text() {
+        let test_string = r#"
+        Aujourd'hui, maman est morte. Ou peut
+    être hier, je ne sais pas. J'ai reçu un télégramme de l'asile : « Mère décédée. Enterrement demain. Sentiments distingués.»
+    Cela ne veut rien dire. C'était peut être
+    hier.
+        "#;
+
         let mut token_stream = get_quickwit_tokenizer_manager()
             .get("log")
             .unwrap()
@@ -237,18 +275,7 @@ mod tests {
             "ssh2",
         ];
 
-        let mut token_stream = get_quickwit_tokenizer_manager()
-            .get("log")
-            .unwrap()
-            .token_stream(test_string);
-
-        array_ref.iter().for_each(|ref_token| {
-            if token_stream.advance() {
-                assert_eq!(&token_stream.token().text, ref_token)
-            } else {
-                panic!()
-            }
-        });
+        log_tokenizer_test_helper(test_string, &array_ref)
     }
 
     #[test]
@@ -256,6 +283,7 @@ mod tests {
         let test_string = "1331901000.000000    CHEt7z3AzG4gyCNgci    192.168.202.79    50465    \
                            192.168.229.251    80    1    HEAD 192.168.229.251    /DEASLog02.nsf    \
                            -    Mozilla/5.0";
+
         let array_ref: [&str; 11] = [
             "1331901000.000000",
             "CHEt7z3AzG4gyCNgci",
@@ -270,24 +298,14 @@ mod tests {
             "Mozilla/5.0",
         ];
 
-        let mut token_stream = get_quickwit_tokenizer_manager()
-            .get("log")
-            .unwrap()
-            .token_stream(test_string);
-
-        array_ref.iter().for_each(|ref_token| {
-            if token_stream.advance() {
-                assert_eq!(&token_stream.token().text, ref_token)
-            } else {
-                panic!()
-            }
-        });
+        log_tokenizer_test_helper(test_string, &array_ref)
     }
 
     #[test]
     fn log_tokenizer_log_test_http() {
         let test_string = "{\"message\" : \"211.11.9.0 - - [1998-06-21T15:00:01-05:00] \"GET \
                            /english/index.html HTTP/1.0\" 304 0\"}";
+
         let array_ref: [&str; 8] = [
             "message",
             "211.11.9.0",
@@ -298,18 +316,8 @@ mod tests {
             "304",
             "0",
         ];
-        let mut token_stream = get_quickwit_tokenizer_manager()
-            .get("log")
-            .unwrap()
-            .token_stream(test_string);
 
-        array_ref.iter().for_each(|ref_token| {
-            if token_stream.advance() {
-                assert_eq!(&token_stream.token().text, ref_token)
-            } else {
-                panic!()
-            }
-        });
+        log_tokenizer_test_helper(test_string, &array_ref)
     }
 
     #[test]
@@ -327,55 +335,48 @@ mod tests {
             "12.32.75.221"
         ];
 
-        let mut token_stream = get_quickwit_tokenizer_manager()
-            .get("log")
-            .unwrap()
-            .token_stream(test_string);
+        log_tokenizer_test_helper(test_string, &array_ref)
+    }
 
-        array_ref.iter().for_each(|ref_token| {
-            if token_stream.advance() {
-                assert_eq!(&token_stream.token().text, ref_token)
-            } else {
-                panic!()
-            }
-        });
+    #[test]
+    fn log_tokenizer_paths_test() {
+        let test_string = r"./quickwit/quickwit-doc-mapper/src/tokenizers.rs
+            /endpoint/index.html
+            /bin/sh src/bin/ test_files.cc 
+
+            peut-etre.out";
+
+        let array_ref: [&str; 6] = [
+            "./quickwit/quickwit-doc-mapper/src/tokenizers.rs",
+            "/endpoint/index.html",
+            "/bin/sh",
+            "src/bin/",
+            "test_files.cc",
+            "peut-etre.out",
+        ];
+
+        log_tokenizer_test_helper(test_string, &array_ref)
     }
 
     #[test]
     fn log_tokenizer_log_wsa() {
-        let test_string = "54.36.149.41 - - [22/Jan/2019:03:56:14 +0330] \"GET /filter/27|13%20%D9%85%DA%AF%D8%A7%D9%BE%DB%8C%DA%A9%D8%B3%D9%84,27|%DA%A9%D9%85%D8%AA%D8%B1%20%D8%A7%D8%B2%205%20%D9%85%DA%AF%D8%A7%D9%BE%DB%8C%DA%A9%D8%B3%D9%84,p53 HTTP/1.1\" 200 30577 \"-\" \"Mozilla/5.0 (compatible; AhrefsBot/6.1; +http://ahrefs.com/robot/)\" \"-\"";
+        let test_string = "54.36.149.41 - - [22/Jan/2019:03:56:14 +0330] \"GET /filter/27 HTTP/1.1\" 200 30577 \"-\" \"Mozilla/5.0 (compatible; AhrefsBot/6.1; +http://ahrefs.com/robot/)\" \"-\"";
 
-        let array_ref: [&str; 16] = [
+        let array_ref: [&str; 12] = [
             "54.36.149.41",
             "22/Jan/2019:03:56:14",
             "0330",
             "GET",
             "/filter/27",
-            "13%20%D9%85%DA%AF%D8%A7%D9%BE%DB%8C%DA%A9%D8%B3%D9%84",
-            "27",
-            "DA%A9%D9%85%D8%AA%D8%B1%20%D8%A7%D8%B2%205%20%D9%85%DA%AF%D8%A7%D9%BE%DB%8C%DA%A9%D8%\
-                B3%D9%84",
-                "p53",
-                "HTTP/1.1",
-                "200",
-                "30577",
-                "Mozilla/5.0",
-                "compatible",
-                "AhrefsBot/6.1",
-                "http://ahrefs.com/robot/",
+            "HTTP/1.1",
+            "200",
+            "30577",
+            "Mozilla/5.0",
+            "compatible",
+            "AhrefsBot/6.1",
+            "http://ahrefs.com/robot/",
         ];
 
-        let mut token_stream = get_quickwit_tokenizer_manager()
-            .get("log")
-            .unwrap()
-            .token_stream(test_string);
-
-        array_ref.iter().for_each(|ref_token| {
-            if token_stream.advance() {
-                assert_eq!(&token_stream.token().text, ref_token)
-            } else {
-                panic!()
-            }
-        });
+        log_tokenizer_test_helper(test_string, &array_ref)
     }
 }
